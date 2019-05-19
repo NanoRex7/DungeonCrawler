43e1eee29d9a6b9d85e48b5c8bf38ac9aa0f30e1
JordanSlater <32441504+JordanSlater@users.noreply.github.com>
2019-05-19 18:40:16 -0400

    Added a moving player, merged Tilemap.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Called before 
+    void Update() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical) * speed;
+        rb2d.velocity = movement;
+    }
+}
