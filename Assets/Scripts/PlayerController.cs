83b610d055fde09f816582fceca389a1f5f2f44f
JordanSlater <32441504+JordanSlater@users.noreply.github.com>
2019-05-28 22:54:05 -0400

    Added relative movement feedback.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
-    public float speed;
+    private Vector2 velocity;
+    public float relativeSpeedFeedback; // 0.01f in java game
+    public float traction; // 0.01f in java game
+    public float maxSpeed; // 0.04f in java game
 
     void Start() {
         rb2d = GetComponent<Rigidbody2D>();
+        rb2d.drag = 0;
+        velocity = new Vector2();
     }
 
     // Called before 
     void Update() {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical) * speed;
-        rb2d.velocity = movement;
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        // raw directional input from the player
+        Vector2 direction = new Vector2(moveHorizontal, moveVertical).normalized;
+        direction += (-velocity * relativeSpeedFeedback / maxSpeed);
+        direction.Normalize();
+
+        velocity += direction * traction;
+
+        if (velocity.magnitude > maxSpeed)
+            velocity *= maxSpeed / velocity.magnitude;
+        if (velocity.magnitude < 1E-4)
+            velocity.Set(0, 0);
+
+        rb2d.MovePosition(rb2d.position + velocity);
     }
 }
