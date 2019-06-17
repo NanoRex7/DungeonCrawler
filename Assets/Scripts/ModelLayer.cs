28233e67454d024a2a02facb8e7c5d9067dea770
NanoRex <djlneuman@gmail.com>
2019-06-17 18:25:21 -0400

    Sprite sheets now share a global indexing system rather than each object having its own copy, objects will now update properly when their sprite sheets are changed in the inspector or with code

--- Assets/Scripts/ModelLayer.cs
+++ Assets/Scripts/ModelLayer.cs
@@ -14,7 +14,8 @@ using System.Text.RegularExpressions;
 public class ModelLayer : MonoBehaviour
 {
     // The sprite sheet to be rendered
-    public Texture2D spriteSheet; // todo: add setter for this?
+    public Texture2D spriteSheet;
+    private Texture2D prevSpriteSheet;
 
     // Property that is animated by the animation clip
     [HideInInspector]
@@ -23,72 +24,18 @@ public class ModelLayer : MonoBehaviour
     // 2D array of sprite references (same as sprite sheet)
     private Sprite[,] sprites;
 
-    // Number of rotations for this sprite (should be = sprites.GetLength(1))
-    private int rotationCount;
-
     // Variables to keep track of state
     private int rotationIndex, prevRotationIndex;
     private int animationFrame, prevAnimationFrame;
 
+
+    private static Dictionary<string, Sprite[,]> spriteDatabase;
+
+
     private void OnEnable()
     {
-        // Workaround for editor mode
-        if (!Application.isPlaying)
-            Awake();
-    }
-
-    void Awake()
-    {
-        // Skip if no sprite sheet is attached
-        if (spriteSheet == null) return;
-
-        // Skip if already initialized (temp)
-        if (sprites != null) return;
-
-        //Assert sprite sheet name is valid _xT
-        if (!Regex.Match(spriteSheet.name, @"_\d+T$").Success) return;
-
-        // Construct 2D array of sprite references based on name
-
-        string path = AssetDatabase.GetAssetPath(spriteSheet);
-
-        Object[] sprites1d = 
-            AssetDatabase.LoadAllAssetRepresentationsAtPath(path);
-
-        Vector2Int[] coords = new Vector2Int[sprites1d.Length];
-        
-        Vector2Int spriteSheetSize = new Vector2Int(0, 0);
-
-        // Search for sprite coordinates, store, and find maximum
-        for (int i = 0; i < sprites1d.Length; i++)
-        {
-            Match num = Regex.Match(sprites1d[i].name, @"\d+",
-                RegexOptions.RightToLeft);
-            int y = int.Parse(num.Value);
-            num = num.NextMatch();
-            int x = int.Parse(num.Value);
-            coords[i] = new Vector2Int(x, y);
-
-            if (x >= spriteSheetSize.x)
-                spriteSheetSize.x = x + 1;
-            if (y >= spriteSheetSize.y)
-                spriteSheetSize.y = y + 1;
-        }
-
-        sprites = new Sprite[spriteSheetSize.x, spriteSheetSize.y];
-
-        // Populate the 2D array
-        for (int i = 0; i < sprites1d.Length; i++)
-            sprites[coords[i].x, coords[i].y] = (Sprite)sprites1d[i];
-
-        // Set the rotation count equal to the number of sprites on the y axis
-        rotationCount = spriteSheetSize.y;
-
-        /*
-        Debug.Log("Model initializing: " + spriteSheet.name + ", " + 
-            spriteSheetSize.x + "x" + spriteSheetSize.y + ", " 
-            + sprites.Length + " sprites");
-        */
+        UpdateSpriteSheet();
+        UpdateSprite();
     }
 
     // Start is called before the first frame update
@@ -100,6 +47,15 @@ public class ModelLayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Detect changes in sprite sheet
+        if (spriteSheet != prevSpriteSheet)
+        {
+            UpdateSpriteSheet();
+            UpdateSprite();
+        }
+
+        prevSpriteSheet = spriteSheet;
+
         // Do not execute if no sprite sheet is set
         if (spriteSheet == null)
             return;
@@ -114,6 +70,9 @@ public class ModelLayer : MonoBehaviour
         // Get angle from parent (model root)
         float angle = transform.parent?.eulerAngles.z ?? 0;
 
+        // Set the rotation count equal to the number of sprites on the y axis
+        int rotationCount = sprites.GetLength(1);
+
         // Convert angle to fraction
         rotationIndex = Mathf.RoundToInt(angle / 360.0f * rotationCount);
         rotationIndex %= rotationCount;
@@ -125,8 +84,7 @@ public class ModelLayer : MonoBehaviour
             animationFrame < sprites.GetLength(0) &&
             rotationIndex < sprites.GetLength(1))
         {
-            GetComponent<SpriteRenderer>().sprite =
-                sprites[animationFrame, rotationIndex];
+            UpdateSprite();
         }
     }
     
@@ -134,5 +92,102 @@ public class ModelLayer : MonoBehaviour
     {
         // Lock rotation
         transform.rotation = Quaternion.identity;
+    }
+
+    /*
+     * Updates the sprite property of the attached sprite renderer
+     */
+    private void UpdateSprite()
+    {
+        GetComponent<SpriteRenderer>().sprite =
+            sprites?[animationFrame, rotationIndex];
+    }
+
+    /*
+     * Performs the necessary updates that must occur when the sprite sheet 
+     * property is changed
+     */
+    private void UpdateSpriteSheet()
+    {
+        // Ensure sprite sheet texture is valid
+        if (spriteSheet != null &&
+            Regex.Match(spriteSheet.name, @"_\d+T$").Success)
+        {
+            sprites = GetSprites(spriteSheet);
+        }
+        else
+        {
+            spriteSheet = null;
+            sprites = null;
+        }
+    }
+
+    /*
+     * Retrieves a 2D array of sprites on a sprite sheet or generates such an
+     * array if it does not exist for a given sprite sheet
+     */
+    private static Sprite[,] GetSprites(Texture2D spriteSheet)
+    {
+        // Initialize dictionary if not initialized
+        if (spriteDatabase == null)
+            spriteDatabase = new Dictionary<string, Sprite[,]>();
+
+        // Use path to identify sprites
+        string assetPath = AssetDatabase.GetAssetPath(spriteSheet);
+
+        // If sprite has already been indexed, fetch it
+        if (spriteDatabase.ContainsKey(assetPath))
+        {
+            return spriteDatabase[assetPath];
+        }
+
+        // Index the sprite by constructing 2D array of sprite references
+        else
+        {
+            // 1D Array of all sprites for the sprite sheet
+            Object[] sprites1d =
+                AssetDatabase.LoadAllAssetRepresentationsAtPath(assetPath);
+
+            // Parallel array of coordinates of a sprite on the sprite sheet
+            Vector2Int[] coords = new Vector2Int[sprites1d.Length];
+
+            // Size of sprite sheet
+            Vector2Int spriteSheetSize = new Vector2Int(0, 0);
+
+            // Search for sprite coordinates, store, and find maximum
+            for (int i = 0; i < sprites1d.Length; i++)
+            {
+                Match num = Regex.Match(sprites1d[i].name, @"\d+",
+                    RegexOptions.RightToLeft);
+                int y = int.Parse(num.Value);
+                num = num.NextMatch();
+                int x = int.Parse(num.Value);
+                coords[i] = new Vector2Int(x, y);
+
+                if (x >= spriteSheetSize.x)
+                    spriteSheetSize.x = x + 1;
+                if (y >= spriteSheetSize.y)
+                    spriteSheetSize.y = y + 1;
+            }
+
+            Sprite[,] sprites = new Sprite[
+                spriteSheetSize.x, 
+                spriteSheetSize.y];
+
+            // Populate the 2D array
+            for (int i = 0; i < sprites1d.Length; i++)
+                sprites[coords[i].x, coords[i].y] = (Sprite)sprites1d[i];
+
+            // Add to the database
+            spriteDatabase.Add(assetPath, sprites);
+            
+            /*
+            Debug.Log("Indexing sprites: " + spriteSheet.name + ", " + 
+                spriteSheetSize.x + "x" + spriteSheetSize.y + ", " 
+                + sprites.Length + " sprites");
+            */
+
+            return sprites;
+        }
     }
 }
