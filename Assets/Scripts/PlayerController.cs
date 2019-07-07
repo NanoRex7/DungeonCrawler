6cbf6cd3e2ce8018747f838f7fdffdb35fd4d3f4
JordanSlater <32441504+JordanSlater@users.noreply.github.com>
2019-07-06 21:59:26 -0400

    Added relative speed feedback

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,22 +8,40 @@ public class PlayerController : MonoBehaviour
 
     public int speed;
     public float walkAnimSpeedFactor;
+    public float relativeSpeedFeedback;
+    public float traction;
+    public float maxSpeed;
+
+    private Vector2 velocity;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        body.drag = 0;
+        velocity = new Vector2();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Set velocity
-        body.velocity = speed * 
-            new Vector2(
-                Input.GetAxis("Horizontal"), 
-                Input.GetAxis("Vertical"))
-                .normalized;
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        // raw directional input from the player
+        Vector2 direction = new Vector2(moveHorizontal, moveVertical).normalized;
+
+        direction += (-velocity * relativeSpeedFeedback / maxSpeed);
+        direction.Normalize();
+
+        velocity += direction * traction;
+
+        if (velocity.magnitude > maxSpeed)
+            velocity *= maxSpeed / velocity.magnitude;
+        if (velocity.magnitude < 1)
+            velocity.Set(0, 0);
+
+        body.velocity = velocity;
 
         // Set animation speed
         foreach (Animator a in GetComponentsInChildren<Animator>())
