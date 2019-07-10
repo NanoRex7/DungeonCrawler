3ebf9c01ad9cd7d6206d134ed4fe941fd313eed4
NanoRex <djlneuman@gmail.com>
2019-07-09 23:14:06 -0400

    Added LockRotation script

---
+++ Assets/Scripts/LockRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Locks the rotation of a game object
+[ExecuteAlways]
+public class LockRotation : MonoBehaviour
+{
+    private void FixedUpdate() { Lock(); }
+
+    private void Update() { Lock(); }
+
+    private void LateUpdate() { Lock(); }
+
+    private void Lock() { transform.rotation = Quaternion.identity; }
+}
